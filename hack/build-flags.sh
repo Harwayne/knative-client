ecb7ee2425fe4064c6603e07da540732e19a342c
Adam Harwayne <harwayne@google.com>
2019-08-16 11:28:49 -0700

    Add -dirty to the Git Revision if there is something changed in the repo when compiling.

--- hack/build-flags.sh
+++ hack/build-flags.sh
@@ -16,6 +16,9 @@ function build_flags() {
   local base="${1}"
   local now="$(date -u '+%Y-%m-%d %H:%M:%S')"
   local rev="$(git rev-parse --short HEAD)"
+  if [[ -n "$(git status --short)" ]]; then
+    rev+="-dirty"
+  fi
   local pkg="github.com/knative/client/pkg/kn/commands"
   local version="${TAG:-}"
   # Use vYYYYMMDD-local-<hash> for the version string, if not passed.
